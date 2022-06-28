bbbd573db58ca640e54013ce871cdc881b6c6f89
Manav Sharma <72880013+Manav112@users.noreply.github.com>
2022-06-28 16:07:54 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -6,9 +6,9 @@ driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
 driver.get('https://nbl.one/login')
 driver.maximize_window()
 
-driver.find_element(By.XPATH, """//input[@type='email']""").send_keys("test.hackerone08@gmail.com")
+driver.find_element(By.XPATH, """//input[@type='email']""").send_keys("email")
 driver.find_element(By.XPATH, """//button[@id="login-custom"]""").click()
 time.sleep(2)
-driver.find_element(By.XPATH, """//input[@type='password']""").send_keys("1234567890")
+driver.find_element(By.XPATH, """//input[@type='password']""").send_keys("password")
 driver.find_element(By.XPATH, """//button[@id="login-custom"]""").click()
 
